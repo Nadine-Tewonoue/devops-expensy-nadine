4e525491c8fd41728e0435e0974630af7c6bf9e3
Nadine-Tewonoue <anne.tewonoue@aims-cameroon.org>
2026-02-14 15:31:21 +0100

    new update

---
+++ apply-yaml.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+kubectl apply -f ./k8s/namespace.yaml
+kubectl apply -f ./k8s/secrets.yaml.template
+kubectl apply -f ./k8s/mongo-pvc.yaml
+kubectl apply -f ./k8s/mongo.yaml
+kubectl apply -f ./k8s/redis.yaml
+kubectl apply -f ./k8s/backend.yaml
+kubectl apply -f ./k8s/frontend.yaml
+kubectl apply -f ./k8s/ingress.yaml
