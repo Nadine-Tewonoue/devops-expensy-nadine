af37fb708f51ac7e5499b618a7aac268becca92e
Nadine-Tewonoue <anne.tewonoue@aims-cameroon.org>
2026-02-21 11:26:53 +0100

    new pipeline

--- docker.sh
+++ docker.sh
@@ -25,11 +25,11 @@ docker push anne2693/worker-app
 docker network create frontend
 docker network create backend
 
-docker run -d --name voting-app --network frontend -p 5000:80 anne2693/voting-app
-docker run -d --name redis --network frontend redis:latest
-docker run -d --name db --network backend -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=postgres postgres:latest
-docker run -d --name worker-app --network frontend --network backend anne2693/worker-app
-docker run -d --name result-app --network backend -p 5001:80 anne2693/result-app
+#docker run -d --name voting-app --network frontend -p 5000:80 anne2693/voting-app
+#docker run -d --name redis --network frontend redis:latest
+#docker run -d --name db --network backend -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=postgres postgres:latest
+#docker run -d --name worker-app --network frontend --network backend anne2693/worker-app
+#docker run -d --name result-app --network backend -p 5001:80 anne2693/result-app
 
 
 
